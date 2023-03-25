bcf76834f00b32bf6735ad4708bec45279a5cb71
davinaa018 <davinaa018@gmail.com>
2023-03-25 13:02:21 -0600

    feature: Added ToolBar and ComboBox

--- OmokUI.java
+++ OmokUI.java
@@ -1,15 +1,20 @@
 package HW4_GUI;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JToolBar;
 
 public class OmokUI extends JFrame {
     
@@ -28,22 +33,29 @@ public class OmokUI extends JFrame {
     private void configureUI(){
         setLayout(new BorderLayout());
 
+        JPanel North = new JPanel();
+        North.setLayout(new BorderLayout());
 
         // Menu
-        JPanel menu = createMenu();
-        add(menu, BorderLayout.NORTH);
+        createMenu();
+
+        // ToolBar
+        JPanel toolBar = createToolBar();
+        North.add(toolBar, BorderLayout.NORTH);
+
+        // ComboBox
+        JPanel comboBox = createComboBox();
+        North.add(comboBox);
 
         // Board
         JPanel board = createBoard();
         add(board, BorderLayout.CENTER);
 
-
-
+        add(North, BorderLayout.NORTH);
 
     }
 
-    private JPanel createMenu(){
-        JPanel menus = new JPanel();
+    private void createMenu(){
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Game");
 
@@ -63,7 +75,34 @@ public class OmokUI extends JFrame {
 
         setJMenuBar(menuBar);
         setVisible(true);
-        return menus;
+        setJMenuBar(menuBar);
+    }
+
+    private JPanel createToolBar(){
+        JPanel panel = new JPanel();
+        JToolBar toolBar = new JToolBar();
+        toolBar.setFloatable(true);
+        toolBar.addSeparator();
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        JButton button1 = new JButton("Play");
+        button1.addActionListener(e -> JOptionPane.showConfirmDialog(null, "Do you want to start a new game?", "New Game", JOptionPane.YES_NO_OPTION));
+        JButton button2 = new JButton("About");
+        toolBar.add(button1);
+        toolBar.add(button2);
+
+        panel.add(toolBar);
+        return panel;
+    }
+
+    private JPanel createComboBox(){
+        JPanel panel = new JPanel();
+        JButton play = new JButton("Play");
+        panel.add(play);
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        panel.add(new JLabel(" Opponent:"));
+        var comboBox = new JComboBox<>(new String[] { "Human", "Computer" });
+        panel.add(comboBox);
+        return panel;
     }
 
 
