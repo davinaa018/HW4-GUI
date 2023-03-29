a94881223f3cb75a8b0e4df1732ff5935dd78491
davinaa018 <davinaa018@gmail.com>
2023-03-29 10:43:24 -0600

    feature: Added startGame method and added Images for button Icons

--- OmokUI.java
+++ OmokUI.java
@@ -3,8 +3,11 @@ package HW4_GUI;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ItemEvent;
 import java.awt.event.KeyEvent;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -18,15 +21,18 @@ import javax.swing.JToolBar;
 
 public class OmokUI extends JFrame {
     
+    private static final String IMAGE_DIR = "images/";
     private BoardPanel boardPanel;
+    private Board board;
 
     public OmokUI(Board board){
         super("Omok Game");
+        this.board = board;
         boardPanel = new BoardPanel(board);
         add(boardPanel);
         configureUI();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(420, 500);
         setVisible(true);
     }
 
@@ -35,23 +41,20 @@ public class OmokUI extends JFrame {
 
         JPanel North = new JPanel();
         North.setLayout(new BorderLayout());
-
         // Menu
         createMenu();
-
         // ToolBar
         JPanel toolBar = createToolBar();
         North.add(toolBar, BorderLayout.NORTH);
-
         // ComboBox
         JPanel comboBox = createComboBox();
         North.add(comboBox);
-
+        North.add(boardPanel.getJLabel(), BorderLayout.SOUTH);
         // Board
         JPanel board = createBoard();
         add(board, BorderLayout.CENTER);
-
         add(North, BorderLayout.NORTH);
+
 
     }
 
@@ -64,14 +67,21 @@ public class OmokUI extends JFrame {
         menuBar.add(menu);
 
         JMenuItem menuItem1 = new JMenuItem("Play", KeyEvent.VK_N);
+        menuItem1.setIcon(createImageIcon("play.png"));
         JMenuItem menuItem2 = new JMenuItem("About", KeyEvent.VK_N);
         JMenuItem menuItem3 = new JMenuItem("Exit", KeyEvent.VK_N);
+        menuItem2.setIcon(createImageIcon("config.png"));
 
         menu.add(menuItem1);
         menu.add(menuItem2);
         menu.add(menuItem3);
 
-        menuItem1.addActionListener(e -> JOptionPane.showConfirmDialog(null, "Do you want to start a new game?", "New Game", JOptionPane.YES_NO_OPTION));
+        menuItem1.addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(null, "Do you want to start a new game?", "Omok", JOptionPane.YES_NO_OPTION);
+            if(result == JOptionPane.YES_OPTION){
+                startGame();
+            }
+        });
 
         setJMenuBar(menuBar);
         setVisible(true);
@@ -84,9 +94,19 @@ public class OmokUI extends JFrame {
         toolBar.setFloatable(true);
         toolBar.addSeparator();
         panel.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        JButton button1 = new JButton("Play");
-        button1.addActionListener(e -> JOptionPane.showConfirmDialog(null, "Do you want to start a new game?", "New Game", JOptionPane.YES_NO_OPTION));
-        JButton button2 = new JButton("About");
+        JButton button1 = new JButton(createImageIcon("play.png"));
+        button1.setToolTipText("Play a new Game");
+        button1.setFocusPainted(false);
+        button1.addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(null, "Do you want to start a new game?", "Omok", JOptionPane.YES_NO_OPTION);
+            if(result == JOptionPane.YES_OPTION){
+                startGame();
+            }
+        } );
+
+        JButton button2 = new JButton(createImageIcon("config.png"));
+        button2.setToolTipText("About");
+        button2.setFocusPainted(false);
         toolBar.add(button1);
         toolBar.add(button2);
 
@@ -97,10 +117,17 @@ public class OmokUI extends JFrame {
     private JPanel createComboBox(){
         JPanel panel = new JPanel();
         JButton play = new JButton("Play");
+        play.addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(null, "Do you want to start a new game?", "Omok", JOptionPane.YES_NO_OPTION);
+            if(result == JOptionPane.YES_OPTION){
+                startGame();
+            }
+        });
         panel.add(play);
         panel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
         panel.add(new JLabel(" Opponent:"));
         var comboBox = new JComboBox<>(new String[] { "Human", "Computer" });
+        
         panel.add(comboBox);
         return panel;
     }
@@ -114,7 +141,21 @@ public class OmokUI extends JFrame {
         return board;
     }
 
+    private void startGame(){
+        board.initializeBoard();
+        boardPanel.placeStone(true);
+        boardPanel.hoveringEffect(true);
+    }
 
+
+    /** Create an image icon from the given image file. */
+    private ImageIcon createImageIcon(String filename) {
+        URL imageUrl = getClass().getResource(IMAGE_DIR + filename);
+        if (imageUrl != null) {
+            return new ImageIcon(imageUrl);
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
         Board board = new Board(15);
